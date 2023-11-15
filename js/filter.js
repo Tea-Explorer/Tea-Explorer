234794edf428263fa04b78bfa7564b8ce2764939
Ugo Obiako <obiakou@yahoo.com>
2023-11-15 14:00:13 -0800

    Updated js file

--- js/filter.js
+++ js/filter.js
@@ -24,36 +24,6 @@ case "Math &151":
  message += "<p>Prerequisite(s): Math &142 or placement test." + "</br>" + "</br>" + "You need to take more Math classes." + 
  "</br>" + "Next class to take: Math &152 - Calculus II.</p>";
  break;
-
-case "Math &152":
- message += "<p>Prerequisite(s): Math &151 or placement test." + "</br>" + "</br>" + "You need to take more Math classes." + 
- "</br>" + "Next class to take: Math &163 - Calculus III.</p>";
- break; 
- 
-case "Math &163":
- message += "<p>Prerequisite(s): Math &152 or instructor permission." + "</br>" + "</br>" + "You need to take more Math classes." + 
- "</br>" + "Next class to take: Math 220 (Linear Algebra), Math 224 (Vector Calculus) and Math 238 (Differential Equations) in any order.</p>";
- break; 
- 
-case "Math 211":
- message += "<p>Prerequisite(s): Math 116 or Math &141 or placement test." + "</br>" + "Note: Math 116 is usually taken by Business and Social Sciences students." + "</br>" +
- "</br>" + "Next class to take: Math 220 (Linear Algebra), Math 224 (Vector Calculus) and Math 238 (Differential Equations) in any order to complete the 200-level Math classes.</p>";
- break; 
- 
-case "Math 220":
- message += "<p>Prerequisite(s): Math &163 or instructor permission." + "</br>" + 
- "</br>" + "Next class to take: Math 211 (Elements of Statistical Methods), Math 224 (Vector Calculus) and Math 238 (Differential Equations) in any order to complete the 200-level Math classes.</p>";
- break; 
- 
-case "Math 224":
- message += "<p>Prerequisite(s): Math &163." + "</br>" + 
- "</br>" + "Next class to take: Math 211 (Elements of Statistical Methods), Math 220 (Linear Algebra) and Math 238 (Differential Equations) in any order to complete the 200-level Math classes.</p>";
- break;
- 
-case "Math 238":
- message += "<p>Prerequisite(s): Math &152 or higher or instructor permission." + "</br>" + "Math &163 strongly recommended." + "</br>" + 
- "</br>" + "Next class to take: Math 211 (Elements of Statistical Methods), Math 220 (Linear Algebra) and Math 224 (Vector Calculus) in any order to complete the 200-level Math classes.</p>";
- break;
  
  default:
 
@@ -63,8 +33,3 @@ document.getElementById("output").innerHTML = message;
 }
 
 
-
-function resetImage(){
-    // eslint-disable-next-line no-undef
-    document.getElementById("selectTea").value = teaType;
-}
